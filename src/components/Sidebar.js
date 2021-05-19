b6994fbedcf479bb89b9b3a014e4b903fb6ecc29
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-05-19 23:00:06 +0300

    ul padding

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,13 +8,13 @@ export default () => {
                     <span className="sidebar-item-text">Renk</span>
                 </div>
                 <div className="sidebar-menu-item">
-                    <li>
-                        <ul><a className="sidebar-menu-items" href="#">Label Text (4)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Lacivert (3)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Sarı (1)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Siyah (5)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Beyaz (2)</a></ul>
-                    </li>
+                    <ul>
+                        <li><a className="sidebar-menu-items" href="#">Label Text (4)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Lacivert (3)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Sarı (1)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Siyah (5)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Beyaz (2)</a></li>
+                    </ul>
                 </div>
             </div>
             <div className="sidebar-item">
@@ -22,12 +22,12 @@ export default () => {
                     <span className="sidebar-item-text">Sıralama</span>
                 </div>
                 <div className="sidebar-menu-item">
-                    <li>
-                        <ul><a className="sidebar-menu-items" href="#">En Düşük Fiyat</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">En Yüksek Fiyat</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">En Yeniler (A{'>'}Z)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">En Yeniler (Z{'>'}A)</a></ul>
-                    </li>
+                    <ul>
+                        <li><a className="sidebar-menu-items" href="#">En Düşük Fiyat</a></li>
+                        <li><a className="sidebar-menu-items" href="#">En Yüksek Fiyat</a></li>
+                        <li><a className="sidebar-menu-items" href="#">En Yeniler (A{'>'}Z)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">En Yeniler (Z{'>'}A)</a></li>
+                    </ul>
                 </div>
             </div>
             <div className="sidebar-item">
@@ -35,15 +35,15 @@ export default () => {
                     <span className="sidebar-item-text">Marka</span>
                 </div>
                 <div className="sidebar-menu-item">
-                    <li>
-                        <ul><a className="sidebar-menu-items" href="#">Samsung (4)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Nokia (6)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Apple (3)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">LG (2)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Huawei (6)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">Xiaomi (9)</a></ul>
-                        <ul><a className="sidebar-menu-items" href="#">General Mobile (1)</a></ul>
-                    </li>
+                    <ul>
+                        <li><a className="sidebar-menu-items" href="#">Samsung (4)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Nokia (6)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Apple (3)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">LG (2)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Huawei (6)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">Xiaomi (9)</a></li>
+                        <li><a className="sidebar-menu-items" href="#">General Mobile (1)</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
