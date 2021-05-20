1e42d559010930c9b581a979ed77d85ca1ce1365
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-05-20 22:14:14 +0300

    pagination update

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Context } from '../context/AppContext';
 import helper from '../util/helper';
 
-export default (props) => {
-    const { count } = props;
-    const [currentPage, setCurrentPage] = useState(1);
+export default () => {
     return (
-        count < 12 ? (<></>) : (
-            <div className="pagination-container">
-                <div className="pagination-item pagination-item-start" onClick={() => { }} >
-                    <span className="pagination-text">{'<'}</span>
-                </div>
-                {
-                    helper.createArrayFromLength(count / 12, 1).map((pageCount, index) => (
-                        <div className="pagination-item" onClick={() => { }} key={`${index}${pageCount}`} >
-                            <span className="pagination-text">{pageCount}</span>
+        <Context.Consumer>
+            {
+                ({ totalCount, currentPage, changePage }) => (
+                    totalCount < 12 ? (<></>) : (
+                        <div className="pagination-container">
+                            <div className="pagination-item pagination-item-start" onClick={() => { currentPage - 1 < 1 ? changePage(1) : changePage(currentPage - 1) }} >
+                                <span className="pagination-text">{'<'}</span>
+                            </div>
+                            {
+                                helper.createArrayFromLength(totalCount / 12, 1).map((pageCount, index) => (
+                                    <div className={`pagination-item ${currentPage === pageCount && 'pagination-current-page'}`} onClick={() => { changePage(pageCount); }} key={`${index}${pageCount}`} >
+                                        <span className="pagination-text">{pageCount}</span>
+                                    </div>
+                                ))
+                            }
+                            <div className="pagination-item pagination-item-end" onClick={() => {
+                                const maxPageCount = totalCount / 12;
+                                currentPage + 1 > maxPageCount ? changePage(maxPageCount) : changePage(currentPage + 1);
+                            }}>
+                                <span className="pagination-text">{'>'}</span>
+                            </div>
                         </div>
-                    ))
-                }
-                <div className="pagination-item pagination-item-end" onClick={() => { }}>
-                    <span className="pagination-text">{'>'}</span>
-                </div>
-            </div>
-        )
+                    )
+                )
+            }
+        </Context.Consumer>
     )
 }
