0cc08a6f0f57d6225e27bde4620d34b9a48504c2
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-05-20 22:14:28 +0300

    color order

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,52 +1,61 @@
 import React from 'react';
+import { Context } from '../context/AppContext';
+import helper from '../util/helper';
 
 export default () => {
     return (
-        <div className="sidebar">
-            <div className="sidebar-item">
-                <div className="sidebar-item-title">
-                    <span className="sidebar-item-text">Renk</span>
-                </div>
-                <div className="sidebar-menu-item">
-                    <ul>
-                        <li><a className="sidebar-menu-items" href="#">Label Text (4)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Lacivert (3)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Sarı (1)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Siyah (5)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Beyaz (2)</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="sidebar-item">
-                <div className="sidebar-item-title">
-                    <span className="sidebar-item-text">Sıralama</span>
-                </div>
-                <div className="sidebar-menu-item">
-                    <ul>
-                        <li><a className="sidebar-menu-items" href="#">En Düşük Fiyat</a></li>
-                        <li><a className="sidebar-menu-items" href="#">En Yüksek Fiyat</a></li>
-                        <li><a className="sidebar-menu-items" href="#">En Yeniler (A{'>'}Z)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">En Yeniler (Z{'>'}A)</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="sidebar-item">
-                <div className="sidebar-item-title">
-                    <span className="sidebar-item-text">Marka</span>
-                </div>
-                <div className="sidebar-menu-item">
-                    <ul>
-                        <li><a className="sidebar-menu-items" href="#">Samsung (4)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Nokia (6)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Apple (3)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">LG (2)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Huawei (6)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">Xiaomi (9)</a></li>
-                        <li><a className="sidebar-menu-items" href="#">General Mobile (1)</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
+        <Context.Consumer>
+            {
+                ({ allData }) => (
+                    <div className="sidebar">
+                        <div className="sidebar-item">
+                            <div className="sidebar-item-title">
+                                <span className="sidebar-item-text">Renk</span>
+                            </div>
+                            <div className="sidebar-menu-item">
+                                <ul>
+                                    {
+                                        Object.keys(helper.groupBy(allData, 'color')).map(color => (
+                                            <li key={color}>
+                                                <a className="sidebar-menu-items" href="#">{`${color} (${helper.groupBy(allData, 'color')[color].row.length})`}</a>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="sidebar-item">
+                            <div className="sidebar-item-title">
+                                <span className="sidebar-item-text">Sıralama</span>
+                            </div>
+                            <div className="sidebar-menu-item">
+                                <ul>
+                                    <li><a className="sidebar-menu-items" href="#">En Düşük Fiyat</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">En Yüksek Fiyat</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">En Yeniler (A{'>'}Z)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">En Yeniler (Z{'>'}A)</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="sidebar-item">
+                            <div className="sidebar-item-title">
+                                <span className="sidebar-item-text">Marka</span>
+                            </div>
+                            <div className="sidebar-menu-item">
+                                <ul>
+                                    <li><a className="sidebar-menu-items" href="#">Samsung (4)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">Nokia (6)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">Apple (3)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">LG (2)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">Huawei (6)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">Xiaomi (9)</a></li>
+                                    <li><a className="sidebar-menu-items" href="#">General Mobile (1)</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+        </Context.Consumer>
     );
 }
