df26a5eed1a57d08a95c0950fc5ae34edac0021e
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-05-20 22:13:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Header from './components/Header';
 import Products from './components/Products';
 import Sidebar from './components/Sidebar';
 import SubHeader from './components/SubHeader';
+import { ContextProvider } from './context/AppContext';
 
 const App = () => {
     return (
-        <div className="wrapper">
-            <Header />
-            <SubHeader />
-            <div className="main">
-                <Sidebar />
-                <Products />
+        <ContextProvider>
+            <div className="wrapper">
+                <Header />
+                <SubHeader />
+                <div className="main">
+                    <Sidebar />
+                    <Products />
+                </div>
             </div>
-        </div>
+        </ContextProvider>
     );
 };
 
