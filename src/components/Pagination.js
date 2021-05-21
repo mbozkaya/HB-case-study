92d06dcc44e47afa1e3d7a9c519686a54e5a402f
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-05-22 00:48:04 +0300

    pagination update

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -6,21 +6,21 @@ export default () => {
     return (
         <Context.Consumer>
             {
-                ({ totalCount, currentPage, changePage }) => (
-                    totalCount < 12 ? (<></>) : (
+                ({ currentPage, changePage, filteredProducts }) => (
+                    filteredProducts.length < 12 ? (<></>) : (
                         <div className="pagination-container">
                             <div className="pagination-item pagination-item-start" onClick={() => { currentPage - 1 < 1 ? changePage(1) : changePage(currentPage - 1) }} >
                                 <span className="pagination-text">{'<'}</span>
                             </div>
                             {
-                                helper.createArrayFromLength(totalCount / 12, 1).map((pageCount, index) => (
-                                    <div className={`pagination-item ${currentPage === pageCount && 'pagination-current-page'}`} onClick={() => { changePage(pageCount); }} key={`${index}${pageCount}`} >
-                                        <span className="pagination-text">{pageCount}</span>
+                                helper.createArrayFromLength(filteredProducts.length / 12).map((pageCount, index) => (
+                                    <div className={`pagination-item ${currentPage === (pageCount + 1) && 'pagination-current-page'}`} onClick={() => { changePage((pageCount + 1)); }} key={`${index}${(pageCount + 1)}`} >
+                                        <span className="pagination-text">{(pageCount + 1)}</span>
                                     </div>
                                 ))
                             }
                             <div className="pagination-item pagination-item-end" onClick={() => {
-                                const maxPageCount = totalCount / 12;
+                                const maxPageCount = filteredProducts.length / 12;
                                 currentPage + 1 > maxPageCount ? changePage(maxPageCount) : changePage(currentPage + 1);
                             }}>
                                 <span className="pagination-text">{'>'}</span>
