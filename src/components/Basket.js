a44ba2bc59fca344639c40e12d3e414cf41fa34d
Muhammet Bozkaya <muhammetbozkaya20@gmail.com>
2021-05-23 10:36:45 +0300

    quick basket

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Context } from '../context/AppContext';
 
 export default () => {
+    const [quickBasketOpen, setQuickBasketOpen] = useState(false);
 
     return (
         <Context.Consumer>
             {
-                ({basket}) => (
-                    <div className="basket">
-                        <span className="basket-circle">Sepetim</span>
-                        <div className="basket-count">
-                            {basket.length}
+                ({ basket, removeFromBasket }) => (
+                    <div className="basket-container">
+                        <div className="basket" onClick={() => setQuickBasketOpen(!quickBasketOpen)}>
+                            <span className="basket-circle">Sepetim</span>
+                            <div className="basket-count">
+                                {basket.length}
+                            </div>
                         </div>
+                        {
+                            quickBasketOpen && (
+                                <div className="quick-basket">
+                                    {
+                                        basket.length === 0 && <span className="quick-basket-empty">Sepetinizde hiç ürün yok.</span>
+                                    }
+                                    {
+                                        basket.map(item => (
+                                            <div className="quick-basket-item" key={item.id}>
+                                                <img className="quick-basket-image" src={item.img} />
+                                                <div className="quick-basket-info">
+                                                    <span className="quick-basket-title">{item.title}</span>
+                                                    <button type="button" className="quick-basket-remove" onClick={() => removeFromBasket(item)}>Kaldır</button>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            )
+                        }
+
                     </div>
                 )
             }
